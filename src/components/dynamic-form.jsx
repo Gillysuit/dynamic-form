bc5c26b035a9981e748142ad60df21a9b167fbad
gillysuit <contemporarygil@gmail.com>
2021-03-30 15:51:16 -0700

    Add boilerplate files and packages

---
+++ src/components/dynamic-form.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import "./dynamic-form.css"
+
+const DynamicForm = (props) => {
+  return (
+    <div>
+    hello
+    </div>
+  )
+}
+
+export default DynamicForm;
